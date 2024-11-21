296959024abcfb6730f5769b8097c0edc46b8934
Guillermo Hernandez <guillermohernandez@gmail.com>
2024-11-21 00:13:10 -0600

    1st push

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://guillermohernandez.github.io",
+  integrations: [react(), sitemap()],
+});
