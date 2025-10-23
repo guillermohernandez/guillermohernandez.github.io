508ef93b2f631a213585f272838d50432ce2b359
Guillermo Hernandez <guillermohernandez@gmail.com>
2025-10-23 09:35:43 -0500

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: "https://guillermohernandez.github.io",
+  integrations: [sitemap()],
 });
